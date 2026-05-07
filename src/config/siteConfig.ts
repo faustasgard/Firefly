a341bfdd8dfb443ed574671be69caa931de3d495
faustasgard <15522877385@163.com>
2026-05-07 17:45:24 +0800

    更新内容

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -7,27 +7,27 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "花花の旧作收集馆",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "Faust",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://erogame.space",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"一花一世界，一叶一菩提。欢迎来到花花の旧作收集馆，这里是一个专注于收集和分享各种旧作游戏的博客。无论是经典的视觉小说、冒险游戏，还是那些被遗忘的宝藏，我们都致力于为大家带来丰富的内容和精彩的回忆。加入我们，一起探索那些曾经让我们心动的旧作世界吧！",
 
 	// 站点关键词
 	keywords: [
-		"Firefly",
-		"Fuwari",
-		"Astro",
+		"Faust",
+		"解语花",
+		"galgame",
 		"ACGN",
-		"博客",
-		"技术博客",
-		"静态博客",
+		"二次元",
+		"旧作收集",
+		"资源站",
 	],
 
 	// 主题色
@@ -79,7 +79,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "Firefly",
+		title: "Faust",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
@@ -91,7 +91,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2025-01-01",
+	siteStartDate: "2020-01-01",
 
 	// 站点时区（IANA 时区字符串），用于格式化bangumi、rss里的构建日期时间等等..
 	// 示例："Asia/Shanghai", "UTC", 如果为空，则按照构建服务器的时区进行时区转换
